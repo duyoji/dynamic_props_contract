0c93f901b95fa5f03d22223e869d142562704949
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2018-05-16 14:03:37 +0900

    Removed some comment in 2_deploy_monster.js

--- migrations/2_deploy_monster.js
+++ migrations/2_deploy_monster.js
@@ -1,19 +1,10 @@
 const Monster = artifacts.require("./Monster.sol");
 
-// use latest web3 instead of using old one built-in truffle.
+// Use latest web3 instead of using old one built-in truffle.
 const Web3latest = require('web3');
 const web3latest = new Web3latest();
 
 module.exports = async function(deployer) {
-
-
-  // console.log(web3latest.utils, '@@@@@@@@');
-
-  // User old version web3 (0.20.6)
-  // const keys = ['id', 'name', 'type', 'rarity'].map(key => web3latest.fromUtf8(key, 32));
-  // const values = ['1', 'モンス', 'Fire', 'SSS'].map(value => web3latest.utf8ToHex(value, 32)); // Dummy data
-
-  // user lates version web3 (1.0.0-beta.34)
   const keys = ['id', 'name', 'type', 'rarity'].map(key => web3latest.utils.utf8ToHex(key));
   const values = ['1', 'モンス', 'Fire', 'SSS'].map(value => web3latest.utils.utf8ToHex(value)); // Dummy data
 
@@ -26,11 +17,8 @@ module.exports = async function(deployer) {
 
   console.log('keys====================================');
   const keysFromMethod = await monster.getAllProps();
-
   console.log('hex keys : ', keysFromMethod);
   console.log('utf8 keys : ', keysFromMethod.map(web3latest.utils.hexToUtf8));
-
-
 
   console.log('value===================================');
   const hexValuesPromises = keysFromMethod.map(key => {
